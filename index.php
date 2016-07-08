c6619f149e958e88e84e24bc8fc2f5db5a483fbc
Allen Toland-Barber <allen_t-b@msn.com>
2016-07-07 23:17:19 -0700

    Update index.php

--- index.php
+++ index.php
@@ -9,17 +9,36 @@
         if(preg_match($pattern, $date)) {
             $nat = date("F d, Y", $date);
             $unix = $date;
-        } else {
+        } else if(strtotime($date)) {
             $unix = strtotime($date);
             $nat = date("F d, Y", $unix);
+        } else {
+            $unix = null;
+            $nat = null;
         }
+    
+        $data = array(
+            "unix" => $unix,
+            "natural" => $nat
+            );
+        
+        header("Content-type: application/json");
+        print json_encode($data);
+    } else {
+        ?>
+        <!DOCTYPE html>
+            <html>
+            <head>
+            	<title>Time stamp service</title>
+            </head>
+            <body>
+                <h1>Time Stamp Micro Service</h1>
+                <p>Access this service passing a unix timestamp with: <code>"https://timestamp12.herokuapp.com/?d=1450137600"</code></p>
+                <p>Or a natural date with: <code>"https://timestamp12.herokuapp.com/?d=December%2015,%202015"</code></p>
+            </body>
+            </html>
+                    
+        
+        <?php
     }
-    $data = array(
-        "unix" => $unix,
-        "natural" => $nat
-        );
-    
-    header("Content-type: application/json");
-    print json_encode($data);
-    
 ?>
